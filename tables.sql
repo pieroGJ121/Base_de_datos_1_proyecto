3f56aaec5aaa35ac1f20637d300c45224925a19b
pieroGJ121 <piero.guerrero@utec.edu.pe>
2023-10-31 18:11:32 -0500

    tables.sql: Give reduced varchar size to attributes

--- tables.sql
+++ tables.sql
@@ -1,51 +1,48 @@
 -- Creacion de tablas
 
 CREATE TABLE Usuario(
-	correo VARCHAR(255)  PRIMARY KEY,
-	username VARCHAR(100),
-	contrasenia VARCHAR(100),
-	pais VARCHAR(60),
+	correo VARCHAR(15)  PRIMARY KEY,
+	username VARCHAR(15),
+	contrasenia VARCHAR(10),
+	pais VARCHAR(10),
 	fecha_creacion DATE,
-	imagen_perfil VARCHAR(100),
 	tipo_suscripcion BOOLEAN
 );
 
 CREATE TABLE Playlist(
 	ID INT PRIMARY KEY,
-	correo_usuario  VARCHAR(255) FOREIGN KEY REFERENCES Usuario(correo),
+	correo_usuario  VARCHAR(15) FOREIGN KEY REFERENCES Usuario(correo),
 	fecha_creacion DATE,
 	privacidad BOOLEAN,
-	nombre  VARCHAR(100),
-	descripcion  VARCHAR(400)
+	nombre  VARCHAR(15),
+	descripcion  VARCHAR(40)
 );
 
 CREATE TABLE Contenido(
 	ID INT PRIMARY KEY,
 	fechaLanzamiento DATE,
-	lenguaje VARCHAR(30),
-	nombre VARCHAR(50),
-	portada VARCHAR(100)
+	lenguaje VARCHAR(10),
+	nombre VARCHAR(15),
 );
 
 CREATE TABLE ArtistaMusical(
-	correo_usuario  VARCHAR(255) FOREIGN KEY REFERENCES Usuario(correo),
-	genero_musica VARCHAR(30)
+	correo_usuario  VARCHAR(15) FOREIGN KEY REFERENCES Usuario(correo),
+	genero_musica VARCHAR(10)
 );
 
 CREATE TABLE RedSocial(
-	nombre VARCHAR(30) PRIMARY KEY
+	nombre VARCHAR(15) PRIMARY KEY
 );
 
 CREATE TABLE Evento(
-	nombre VARCHAR(100) PRIMARY KEY,
+	nombre VARCHAR(15) PRIMARY KEY,
 	fecha DATE,
-	lugar VARCHAR(50)	
+	lugar VARCHAR(15)
 );
 
 CREATE TABLE ArtistaPodcast(
-	correo_usuario VARCHAR(255) FOREIGN KEY REFERENCES Usuario(correo)
+	correo_usuario VARCHAR(15) FOREIGN KEY REFERENCES Usuario(correo)
 );
-
 
 
 CREATE TABLE ContenidoAcumulable(
@@ -54,13 +51,8 @@ CREATE TABLE ContenidoAcumulable(
 );
 
 CREATE TABLE Podcast(
-	nombre VARCHAR(30) PRIMARY KEY,
+	nombre VARCHAR(15) PRIMARY KEY,
 	original BOOLEAN
-);
-
-CREATE TABLE Temporada(
-	ID INT FOREIGN KEY REFERENCES Contenido(ID),
-	nombre VARCHAR(30) FOREIGN KEY REFERENCES Podcast(nombre)
 );
 
 CREATE TABLE Album(
@@ -69,47 +61,42 @@ CREATE TABLE Album(
 
 CREATE TABLE Episodio(
 	ID INT FOREIGN KEY REFERENCES Contenido(ID),
-	nombre VARCHAR(30) FOREIGN KEY REFERENCES Podcast(nombre)
+	nombre VARCHAR(15) FOREIGN KEY REFERENCES Podcast(nombre),
+	temporada VARCHAR(15)
 );
 
 CREATE TABLE Cancion(
 	ID INT FOREIGN KEY REFERENCES Contenido(ID),
-	genero  VARCHAR(30),
-	letra  VARCHAR(1000),
-	compositor  VARCHAR(30)
+	genero  VARCHAR(10),
+	compositor  VARCHAR(15)
 );
 
 CREATE TABLE Favoritos(
-	correo VARCHAR(255) FOREIGN KEY REFERENCES Usuario(correo),
+	correo VARCHAR(15) FOREIGN KEY REFERENCES Usuario(correo),
 	ID INT FOREIGN KEY REFERENCES ContenidoAcumulable(ID)
 );
 
 CREATE TABLE AlmacenaPlaylist(
 	IDplaylist INT FOREIGN KEY REFERENCES Playlist(ID),
-	correo VARCHAR(255) FOREIGN KEY REFERENCES Usuario(correo),
+	correo VARCHAR(15) FOREIGN KEY REFERENCES Usuario(correo),
 	ID_CA INT FOREIGN KEY REFERENCES ContenidoAcumulable(ID)
 );
 
 CREATE TABLE Participa(
-	correo VARCHAR(255) FOREIGN KEY REFERENCES ArtistaPodcast(correo_usuario),
-	nombre VARCHAR(30)  FOREING KEY REFERENCES Podcast(Nombre)
+	correo VARCHAR(15) FOREIGN KEY REFERENCES ArtistaPodcast(correo_usuario),
+	nombre VARCHAR(15)  FOREING KEY REFERENCES Podcast(Nombre)
 );
 
 
 CREATE TABLE TieneRedes(
-	correo VARCHAR(255) FOREIGN KEY REFERENCES ArtistaPodcast(correo_usuario),
-	nombre_Red VARCHAR(30) FOREIGN KEY REFERENCES RedSocial(nombre),
-	username VARCHAR(30)
+	correo VARCHAR(15) FOREIGN KEY REFERENCES ArtistaPodcast(correo_usuario),
+	nombre_Red VARCHAR(10) FOREIGN KEY REFERENCES RedSocial(nombre),
+	username VARCHAR(10)
 );
 
 CREATE TABLE TieneEventos(
-	correo VARCHAR(255) FOREIGN KEY REFERENCES ArtistaPodcast(correo_usuario),
-	nombre VARCHAR(100) FOREIGN KEY REFERENCES Evento(nombre)
-);
-
-CREATE TABLE AlmacenaTemporada(
-	ID_Ep INT FOREIGN KEY REFERENCES Episodio(ID),
-	ID_Tmp INT FOREIGN KEY REFERENCES Temporada(ID)
+	correo VARCHAR(15) FOREIGN KEY REFERENCES ArtistaPodcast(correo_usuario),
+	nombre VARCHAR(15) FOREIGN KEY REFERENCES Evento(nombre)
 );
 
 CREATE TABLE AlmacenaAlbum(
@@ -118,17 +105,17 @@ CREATE TABLE AlmacenaAlbum(
 );
 
 CREATE TABLE CreaAlbum(
-	correo VARCHAR(255) FOREIGN KEY REFERENCES ArtistaMusical(correo),
+	correo VARCHAR(15) FOREIGN KEY REFERENCES ArtistaMusical(correo),
 	ID_Ab INT FOREIGN KEY REFERENCES Album(ID)
 );
 
 CREATE TABLE CreaCancion(
-	correo VARCHAR(255) FOREIGN KEY REFERENCES ArtistaMusical(correo),
+	correo VARCHAR(15) FOREIGN KEY REFERENCES ArtistaMusical(correo),
 	ID_Cn INT FOREIGN KEY REFERENCES Cancion(ID)
 );
 
 CREATE TABLE CreaEpisodio(
-	nombre VARCHAR(30) FOREIGN KEY REFERENCES Podcast(Nombre),
+	nombre VARCHAR(15) FOREIGN KEY REFERENCES Podcast(Nombre),
 	ID_Ep INT FOREIGN KEY REFERENCES Episodio(ID)
 );
 
