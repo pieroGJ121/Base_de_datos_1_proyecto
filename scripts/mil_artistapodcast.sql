4e04dd51798e87b848b652da280d6c7c6f2e469e
pieroGj121 <piero.guerrero@utec.edu.pe>
2023-11-03 16:01:51 -0500

    scripts: Add sql scripts for podcast, usuario, artistapodcast and participa

---
+++ scripts/mil_artistapodcast.sql
@@ -0,0 +1,88 @@
+INSERT INTO mil.artistapodcast (correo) VALUES
+	 ('xh@t.com'),
+	 ('u@n.com'),
+	 ('gvvx@nlk.com'),
+	 ('o@bpp.com'),
+	 ('lrvx@rf.com'),
+	 ('mbz@o.com'),
+	 ('g@quap.com'),
+	 ('b@ehm.com'),
+	 ('po@p.com'),
+	 ('j@n.com');
+INSERT INTO mil.artistapodcast (correo) VALUES
+	 ('y@c.com'),
+	 ('s@hnedk.com'),
+	 ('vb@to.com'),
+	 ('fcc@i.com'),
+	 ('v@ns.com'),
+	 ('p@hyf.com'),
+	 ('ywggs@fjell.com'),
+	 ('a@o.com'),
+	 ('x@ewmk.com'),
+	 ('veuny@di.com');
+INSERT INTO mil.artistapodcast (correo) VALUES
+	 ('no@sk.com'),
+	 ('t@bjj.com'),
+	 ('k@j.com'),
+	 ('jt@k.com'),
+	 ('n@m.com'),
+	 ('xubuk@f.com'),
+	 ('p@mp.com'),
+	 ('m@b.com'),
+	 ('x@h.com'),
+	 ('xc@a.com');
+INSERT INTO mil.artistapodcast (correo) VALUES
+	 ('f@dp.com'),
+	 ('rpagp@dqqz.com'),
+	 ('hdi@i.com'),
+	 ('r@fc.com'),
+	 ('ni@ul.com'),
+	 ('m@n.com'),
+	 ('fe@rpt.com'),
+	 ('t@wgz.com'),
+	 ('n@qkkws.com'),
+	 ('egrsl@j.com');
+INSERT INTO mil.artistapodcast (correo) VALUES
+	 ('hb@pymv.com'),
+	 ('cwo@td.com'),
+	 ('yk@fme.com'),
+	 ('f@tqp.com'),
+	 ('bf@wefnm.com'),
+	 ('vj@bg.com'),
+	 ('e@sit.com'),
+	 ('gfepo@rteb.com'),
+	 ('r@bk.com'),
+	 ('c@ncc.com');
+INSERT INTO mil.artistapodcast (correo) VALUES
+	 ('r@u.com'),
+	 ('to@b.com'),
+	 ('a@g.com'),
+	 ('sbet@op.com'),
+	 ('l@hvrye.com'),
+	 ('sqk@t.com'),
+	 ('p@c.com'),
+	 ('c@jlj.com'),
+	 ('z@z.com'),
+	 ('d@n.com');
+INSERT INTO mil.artistapodcast (correo) VALUES
+	 ('ll@q.com'),
+	 ('u@qve.com'),
+	 ('sh@a.com'),
+	 ('f@r.com'),
+	 ('wmn@n.com'),
+	 ('cd@er.com'),
+	 ('z@r.com'),
+	 ('cdpfb@mo.com'),
+	 ('wj@m.com'),
+	 ('n@cf.com');
+INSERT INTO mil.artistapodcast (correo) VALUES
+	 ('exu@j.com'),
+	 ('mxuo@ga.com'),
+	 ('f@t.com'),
+	 ('u@ya.com'),
+	 ('wr@il.com'),
+	 ('h@ss.com'),
+	 ('vw@od.com'),
+	 ('tm@w.com'),
+	 ('lel@ck.com'),
+	 ('l@mh.com');
