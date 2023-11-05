8601fa2c4e34d2ab9c128eee0d7c074948aef37d
pieroGj121 <piero.guerrero@utec.edu.pe>
2023-11-05 12:32:41 -0500

    data.py: Add coments

--- data.py
+++ data.py
@@ -48,31 +48,44 @@ def generate_artista_musical_from_usuarios(n):
 
 def generate_evento(n):
     # Jairo
+    # En mil, n es como 200 y para cada siguiente schema pones otro 0
     i = 0
 
 
 def generate_tiene_evento(n):
     # Jairo
+    # Los que participan son como 5 artistas en promedio por evento.
+    # Para mil hay como 100 astistas musicales, asi que n es como 500. Para
+    # cada siguiente schema pones otro 0
     i = 0
 
 
 def generate_red_social(n):
     # Jairo
+    # Este es fijo. Por cada res social principal que encuentres, lo pones. No
+    # cambia respecto al schmema. El nombre seria el nombre de la red social
     i = 0
 
 
 def generate_tiene_red_social(n):
     # Jairo
+    # Cada artista musical tiene como 2 cuentas. Para mil, hay 100 artistas
+    # musicales, asi que n es 200
     i = 0
 
 
 def generate_playlist(n):
     # Jairo
+    # Para mil, hay como unos 300 usuarios, asi que n es como 400. Para
+    # el siguiente schema pones otro 0
     i = 0
 
 
 def generate_almacena_playlist(n):
     # Jairo
+    # Para mil, hay como unos 400 usuarios, asi que n es como 700. Para
+    # el siguiente schema pones otro 0
+
     i = 0
 
 
@@ -213,16 +226,28 @@ def generate_album(n):
 
 def generate_almacena_album(n):
     # Jairo
+    # Deberian haber varias canciones por album, pero al probar en mi
+    # computadora con favoritos, se puede demorar bastante en craer para el
+    # millon. Para 1000, hora hay como 700 canciones y 70 albums, asi que n
+    # es como 1200. Pones otro 0 para el siguiente schema, pero en el millon
+    # que posiblemente se demore cambias el 12 por un 8. Si quieres tambien
+    # puedes dejarlo por bastante tiempo
     i = 0
 
 
 def generate_crea_album(n):
     # Jairo
+    # Ahora hay al menos un autor para cada album. Haz que n sea como la
+    # tercera parte de albumes que hay. Para 1000, hay 77, asi que n es como
+    # 25. Para cada schema siguiente le pones otro 0
     i = 0
 
 
 def generate_crea_cancion(n):
     # Jairo
+    # Ahora hay al menos un autor para cada cancion. Haz que n sea como la
+    # tercera parte de canciones que hay. Para 1000, hay 700, asi que n es como
+    # 240. Para cada schema siguiente le pones otro 0
     i = 0
 
 
