7346f27c5cf757ee32477171807f037a738f742b
pieroGj121 <piero.guerrero@utec.edu.pe>
2023-11-11 03:46:29 -0500

    load.sh: Add missing files

--- load.sh
+++ load.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
-for table in usuario podcast artistamusical artistapodcast participa contenido contenidoacumulable episodio cancion album almacenaalbum creaalbum creacancion favoritos; do
-    psql -U postgres -d bd1_proyecto -a -f "scripts/mil_$table.sql"
-    psql -U postgres -d bd1_proyecto -a -f "scripts/diezmil_$table.sql"
-    psql -U postgres -d bd1_proyecto -a -f "scripts/cienmil_$table.sql"
-    psql -U postgres -d bd1_proyecto -a -f "scripts/millon_$table.sql"
+for table in usuario artistamusical artistapodcast podcast participa contenido contenidoacumulable episodio cancion album almacenaalbum creaalbum creacancion favoritos playlist almacenaplaylist redsocial tieneredes evento tieneeventos; do
+    for schema in mil diezmil cienmil millon; do
+        psql -U postgres -d bd1_proyecto -a -f "scripts/${schema}_${table}.sql"
+    done
 done
