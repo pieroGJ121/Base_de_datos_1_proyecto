dfd749117918af4b6a98565e52b50206424a459a
pieroGj121 <piero.guerrero@utec.edu.pe>
2023-11-11 15:16:25 -0500

    load.sh: Add remining sql scripts

--- load.sh
+++ load.sh
@@ -5,3 +5,17 @@ for table in usuario artistamusical artistapodcast podcast participa contenido c
         psql -U postgres -d bd1_proyecto -a -f "scripts/${schema}_${table}.sql"
     done
 done
+
+for table in favoritos participa; do
+    for schema in mil diezmil cienmil millon; do
+        psql -U postgres -d bd1_proyecto -a -f "scripts/${schema}_${table}.sql"
+    done
+done
+
+# Deletion of files
+# for script in cancion episodio contenido contenidoacumulable album almacenaalbum creaalbum creacancion favoritos; do
+# for script in podcast participa; do
+#     for table in mil diezmil cienmil millon; do
+#         rm "scripts/${table}_${script}.sql"
+#     done
+# done
