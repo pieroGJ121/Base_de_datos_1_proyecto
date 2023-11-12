7bfd0de6db0fa828c38cb794a7aebc5d424f4194
pieroGj121 <piero.guerrero@utec.edu.pe>
2023-11-11 21:40:21 -0500

    querry.sql: Add P2

--- querry.sql
+++ querry.sql
@@ -9,6 +9,30 @@ ABS(fecha - fechaLanzamiento) <= 180 AND IDA IN
 AND correo IN
 (SELECT correo FROM TieneRedes GROUP BY correo HAVING COUNT(*) >= 4);
 -- P2
+
+-- First find the usuario with the artist throught playlist, then
+-- find the usuario with the artist throught favoritos. Do join on both columns and
+SELECT DISTINCT correo FROM
+(SELECT correo_artista, correo FROM
+(SELECT idc as id, correo as correo_artista FROM CreaCancion
+UNION
+SELECT Episodio.id as id, correo AS correo_artista FROM Episodio
+JOIN Podcast ON Episodio.idp = Podcast.id JOIN Participa ON Podcast.id = Participa.idp
+) Artistas JOIN
+Favoritos ON Artistas.id = Favoritos.id WHERE correo IN
+(SELECT correo From Usuario WHERE tipo_suscripcion = true)
+INTERSECT
+SELECT correo_artista, correo FROM
+(SELECT idc as id, correo as correo_artista FROM CreaCancion
+UNION
+SELECT Episodio.id as id, correo AS correo_artista FROM Episodio
+JOIN Podcast ON Episodio.idp = Podcast.id JOIN Participa ON Podcast.id = Participa.idp
+) Artistas JOIN
+AlmacenaPlaylist ON Artistas.id = AlmacenaPlaylist.idca
+JOIN Playlist ON AlmacenaPlaylist.idp = Playlist.id
+WHERE correo IN
+(SELECT correo From Usuario WHERE tipo_suscripcion = true)) ende;
+
 -- P3
 SELECT DISTINCT correo, username FROM Participa NATURAL JOIN Usuario WHERE IDP IN (SELECT IDP FROM
 (SELECT ID, COUNT(*) as cantidad FROM
