7cdd87c212a13efef02d8e0a0c41d8832872ffeb
pieroGj121 <piero.guerrero@utec.edu.pe>
2023-11-21 02:44:04 -0500

    trigger.sql: Correct

--- trigger.sql
+++ trigger.sql
@@ -15,14 +15,14 @@ CREATE OR REPLACE TRIGGER trigger_actualizarAlmacenarAlbum
  AFTER INSERT ON AlmacenaAlbum
 FOR EACH ROW EXECUTE FUNCTION ActualizarAlmacenaAlbum();
 
--- Deletes the inserted object if the date of creation of the event is after the ArtistaMusical
+-- Deletes the inserted object if the date of creation of the event is before the ArtistaMusical was created
 CREATE OR REPLACE FUNCTION ActualizarTieneEventos() RETURNS TRIGGER AS
 $$
 BEGIN
-IF((SELECT fecha FROM Evento WHERE ID=NEW.ID) <
-(SELECT fecha_creacion FROM ArtistaMusical A JOIN Usuario U ON A.correo = U.correo WHERE A.correo=NEW.correo))
+IF((SELECT fecha FROM Evento WHERE ID = NEW.ID) <
+(SELECT fecha_creacion FROM Usuario WHERE correo = NEW.correo))
 THEN
-DELETE FROM TieneEventos E WHERE E.ID=NEW.ID;
+DELETE FROM TieneEventos WHERE ID = NEW.ID;
 END IF;
 RETURN NEW;
 END;
@@ -32,14 +32,14 @@ CREATE OR REPLACE TRIGGER trigger_actualizarTieneEventos
  AFTER INSERT ON TieneEventos
 FOR EACH ROW EXECUTE FUNCTION ActualizarTieneEventos();
 
--- Deletes the inserted object if the date of creation of the song is after the ArtistaMusical
+-- Deletes the inserted object if the date of creation of the song is after the album
 CREATE OR REPLACE FUNCTION ActualizarAlmacenaAlbumC() RETURNS TRIGGER AS
 $$
 BEGIN
-IF((SELECT fechaLanzamiento FROM Contenido WHERE ID=NEW.ID) <
-(SELECT fecha_creacion FROM ArtistaMusical A JOIN Usuario U ON A.correo = U.correo WHERE A.correo=NEW.correo))
+IF((SELECT fechaLanzamiento FROM Contenido WHERE ID = NEW.IDC) >
+(SELECT fechaLanzamiento FROM Contenido WHERE ID = NEW.IDA))
 THEN
-DELETE FROM AlmacenaAlbum A WHERE A.IDC=NEW.ID;
+DELETE FROM AlmacenaAlbum WHERE IDC = NEW.IDC AND IDA = NEW.IDA;
 END IF;
 RETURN NEW;
 END;
@@ -49,19 +49,38 @@ CREATE OR REPLACE TRIGGER trigger_actualizarAlmacenaAlbum
  AFTER INSERT ON AlmacenaAlbum
 FOR EACH ROW EXECUTE FUNCTION ActualizarAlmacenaAlbumC();
 
--- Deletes the inserted object if the date of creation of the episodio is after the ArtistaMusical
-CREATE OR REPLACE FUNCTION ActualizarCreaEpisodio() RETURNS TRIGGER AS
+-- Deletes the inserted object if the date of creation of the episodio is before the Podcast
+CREATE OR REPLACE FUNCTION ActualizarEpisodio() RETURNS TRIGGER AS
 $$
 BEGIN
-IF((SELECT fechaLanzamiento FROM Contenido WHERE ID=NEW.ID) <
-(SELECT fecha_creacion FROM ArtistaMusical A JOIN Usuario U ON A.correo = U.correo WHERE A.correo=NEW.correo))
+IF((SELECT fechaLanzamiento FROM Contenido WHERE ID = NEW.ID) <
+(SELECT fecha_creacion FROM ArtistaPodcast WHERE ID = NEW.IDP))
 THEN
-DELETE FROM CreaEpisodio CE WHERE CE.ide=NEW.ID;
+DELETE FROM Episodio WHERE ID = NEW.ID;
+DELETE FROM ContenidoAcumulable WHERE ID = NEW.ID;
+DELETE FROM Contenido WHERE ID = NEW.ID;
 END IF;
 RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER trigger_actualizarCreaEpisodio
- AFTER INSERT ON CreaEpisodio
-FOR EACH ROW EXECUTE FUNCTION ActualizarCreaEpisodio();
+CREATE OR REPLACE TRIGGER trigger_actualizarEpisodio
+ AFTER INSERT ON Episodio
+FOR EACH ROW EXECUTE FUNCTION ActualizarEpisodio();
+
+-- Deletes the inserted object if the date of creation of the playlist is before the user
+CREATE OR REPLACE FUNCTION ActualizarPlaylist() RETURNS TRIGGER AS
+$$
+BEGIN
+IF((SELECT fecha_creeacion FROM Playlist WHERE ID=NEW.ID) <
+(SELECT fecha_creacion FROM Usuario WHERE correo=NEW.correo))
+THEN
+DELETE FROM Playlist WHERE ID=NEW.ID;
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trigger_actualizarPlaylist
+ AFTER INSERT ON Playlist
+FOR EACH ROW EXECUTE FUNCTION ActualizarPlaylist();
